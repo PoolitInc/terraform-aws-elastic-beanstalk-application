10d04c86ed43323547a20bbdc61e3a5ff9ab02c9
kmai <kam@penneo.com>
2022-09-22 12:38:27 +0200

    Added extra outputs

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "elastic_beanstalk_application_name" {
   value       = aws_elastic_beanstalk_application.default.name
   description = "Elastic Beanstalk Application name"
 }
+
+output "elastic_beanstalk_application_arn" {
+  value = aws_elastic_beanstalk_application.default.arn
+  description = "Elastic Beanstalk Application ARN"
+}
